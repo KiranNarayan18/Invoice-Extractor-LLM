4bdbbe36b496f0d716ebf962b9ad48e569389df2
Kiran Narayan <kiran.narayan71@gmail.com>
2024-02-18 15:55:43 +0530

    fix: solved auth error

--- extract_text.py
+++ extract_text.py
@@ -5,13 +5,28 @@ from dotenv import load_dotenv
 import google.generativeai as genai
 
 
-
 class ExtractText:
     def __init__(self) -> None:
         
+        load_dotenv()
         genai.configure(api_key = os.getenv('GOOGLE_API_KEY'))
         self.model = genai.GenerativeModel('gemini-pro-vision')
 
+
+    def preprocess(self, uploaded_file):
+
+        if uploaded_file is not None:
+
+            bytes_data = uploaded_file.getvalue()
+
+            image_parts = [
+                {
+                    "mime_type": uploaded_file.type,  # Get the mime type of the uploaded file
+                    "data": bytes_data
+                }
+            ]
+
+            return image_parts
 
     def get_text(self, input, image, prompt):
         try:
